99bf5656acc23681bacd93d003dcf16e7fa2dced
LastWeekNextDay <56513101+LastWeekNextDay@users.noreply.github.com>
2023-10-31 16:58:10 +0200

    Implemented World Save

---
+++ Assets/Scripts/Editor/GlobalWorldScriptableObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GlobalWorld", menuName = "ScriptableObjects/GlobalWorldScriptableObject", order = 1)]
+public class GlobalWorldScriptableObject : ScriptableObject
+{
+    public int ID;
+    //public GlobalWorldInfo Info;
+}
