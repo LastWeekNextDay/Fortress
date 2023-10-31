072eea5e6c9c73429d95304f797650e019771fde
LastWeekNextDay <56513101+LastWeekNextDay@users.noreply.github.com>
2023-10-31 16:18:03 +0200

    Implemented finding tile by ID

--- Assets/Scripts/World/LocalWorld.cs
+++ Assets/Scripts/World/LocalWorld.cs
@@ -58,5 +58,22 @@ public class LocalWorld : MonoBehaviour
             }
         }
     }
+
+    public GameObject FindTileByID(int id)
+    {
+        Debug.Log("Finding tile with ID " + id);
+        for (int x = 0; x < TileCountX; x++)
+        {
+            for (int y = 0; y < TileCountY; y++)
+            {
+                if (Tiles[x, y].GetComponent<Tile>().ID == id)
+                {
+                    Debug.Log("Found tile with ID " + id);
+                    return Tiles[x, y];
+                }
+            }
+        }
+        Debug.LogError("Could not find tile with ID " + id);
+        return null;
     }
 }
