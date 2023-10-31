4785fe5cc1785c8f39cc597e80e1c4493f668bb9
LastWeekNextDay <56513101+LastWeekNextDay@users.noreply.github.com>
2023-10-31 15:19:00 +0200

    Added ID to Tile

--- Assets/Scripts/World/Tile.cs
+++ Assets/Scripts/World/Tile.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    public int ID;
     public float BuildabilityPercentage;
     public float MovementModifierPercentage;
     // Start is called before the first frame update
