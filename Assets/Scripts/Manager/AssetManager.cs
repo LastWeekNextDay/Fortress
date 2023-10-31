64e91674b85b25a31900450ef70ca53d6f4da1f4
LastWeekNextDay <56513101+LastWeekNextDay@users.noreply.github.com>
2023-10-31 14:52:38 +0200

    Created Asset Manager

---
+++ Assets/Scripts/Manager/AssetManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class AssetManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject Spawn(GameObject prefab, Vector3 position)
+    {
+        Debug.Log("Spawning " + prefab.name + " at " + position);
+        return Instantiate(prefab, position, prefab.transform.rotation);
+    }
+
+    public GameObject Spawn(GameObject prefab, Vector3 position, Transform parent)
+    {
+        Debug.Log("Spawning " + prefab.name + " at " + position + " with parent " + parent.name);
+        return Instantiate(prefab, position, prefab.transform.rotation, parent);
+    }
+
+    public T LoadAsset<T>(string name)
+    {
+        AsyncOperationHandle<T> handle = Addressables.LoadAssetAsync<T>(name);
+        handle.WaitForCompletion();
+        return handle.Result;
+    }
+
+    public GameObject LoadTile(TileType type)
+    {
+        switch (type)
+        {
+            case TileType.BLANK:
+                return LoadAsset<GameObject>("BlankTile");
+            default:
+                return null;
+        }
+    }
+}
